7f7ac444ee9401a1947acf807281e0090cb62892
Robil <ronald.binilang@gmail.com>
2020-05-23 21:25:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,9 @@ def add_numbers_post():
 	  # print(type(request.form['text']))	
 	  if request.method == 'GET':
 	  	return render_template('add_numbers.html')
-	  elif request.method == 'POST':
+	  elif request.method == 'POST':	
   	      print(request.form['text'].split())
   	      total = 1
-		i=1
   	      try:
   	      	for str_num in request.form['text'].split():
   	      		total *= int(str_num)
