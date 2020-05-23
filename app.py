b0cd4545aff5cf46119105612c39f2974a230e13
Robil <ronald.binilang@gmail.com>
2020-05-23 19:30:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,16 +31,16 @@ def add_numbers_post():
 	  elif request.method == 'POST':
   	      print(request.form['text'].split())
   	      total = 0
-  	      try:
 		interval_input = request.form['text'].split() #retreived the string input and split them in to'interval_input list 
 		min_number = int(interval_input[0])
 	      	max_number = int(interval_input[1])
 	      	n_series = int(interval_input[-1])
+  	      try:
   	      	#for str_num in request.form['text'].split():
 		for n_num in range (n_series): 
 			min_number, max_number = max_number, min_number+max_number
-			total = min_number
-		#total1 = max_number/min_number
+		total = min_number
+		#total1 =max_number/min_number
   	      		#total *= int(str_num)
   	      	return render_template('add_numbers.html', result=str(total)) #render_template('add_numbers.html', result1=str(total1))
   	      except ValueError:
