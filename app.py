3b70d5fef0033d10baae832841f23c3ca976cd6d
Robil <ronald.binilang@gmail.com>
2020-05-23 21:51:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,11 +32,14 @@ def add_numbers_post():
   	      print(request.form['text'].split())
   	      total = 1
   	      try:
-  	      	for str_num in request.form['text'].split():
-  	      		total *= int(str_num)
+		for str_num in request.form['text'].split:
+			total *= int(str_num)
+  	      	#for str_num in request.form['text'].split():
+  	      		#total *= int(str_num)
   	      	return render_template('add_numbers.html', result=str(total))
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
+
 
 
 @app.route('/shopping_list', methods=['GET','POST'])
